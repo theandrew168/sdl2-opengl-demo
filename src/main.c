fe69398e92b3d9ce68f72a6cb75b9a46ae12917c
theandrew168 <steveday168@gmail.com>
2020-06-07 00:45:53 -0500

    Finish the actual demo code

--- src/main.c
+++ src/main.c
@@ -7,10 +7,70 @@
 
 #include "opengl.h"
 
-enum {
-    WINDOW_WIDTH = 640,
-    WINDOW_HEIGHT = 640,
+#ifndef M_PI
+#define M_PI 3.141592653589793
+#endif
+
+const float SQUARE[] = {
+    -1.0f,  1.0f,
+    -1.0f, -1.0f,
+     1.0f,  1.0f,
+     1.0f, -1.0f
 };
+
+static bool
+opengl_shader_compile_source(GLuint shader, const GLchar* source)
+{
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (success != GL_TRUE) {
+        GLint info_log_length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_log_length);
+
+        GLchar* info_log = malloc(info_log_length);
+        glGetShaderInfoLog(shader, info_log_length, NULL, info_log);
+
+        fprintf(stderr, "failed to compile shader:\n%s\n", info_log);
+        free(info_log);
+
+        return false;
+    }
+
+    return true;
+}
+
+static bool
+opengl_shader_link_program(GLuint program, GLuint vertex_shader, GLuint fragment_shader)
+{
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    GLint success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (success != GL_TRUE) {
+        GLint info_log_length;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &info_log_length);
+
+        GLchar* info_log = malloc(info_log_length);
+        glGetProgramInfoLog(program, info_log_length, NULL, info_log);
+
+        fprintf(stderr, "failed to link program:\n%s\n", info_log);
+        free(info_log);
+
+        glDetachShader(program, vertex_shader);
+        glDetachShader(program, fragment_shader);
+
+        return false;
+    }
+
+    glDetachShader(program, vertex_shader);
+    glDetachShader(program, fragment_shader);
+    return false;
+}
 
 int
 main(int argc, char* argv[])
@@ -19,6 +79,15 @@ main(int argc, char* argv[])
         fprintf(stderr, "failed to init SDL2: %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
+
+    printf("Platform:        %s\n", SDL_GetPlatform());
+    printf("CPU Count:       %d\n", SDL_GetCPUCount());
+    printf("System RAM:      %d MB\n", SDL_GetSystemRAM());
+    printf("Supports SSE:    %s\n", SDL_HasSSE() ? "true" : "false");
+    printf("Supports SSE2:   %s\n", SDL_HasSSE2() ? "true" : "false");
+    printf("Supports SSE3:   %s\n", SDL_HasSSE3() ? "true" : "false");
+    printf("Supports SSE4.1: %s\n", SDL_HasSSE41() ? "true" : "false");
+    printf("Supports SSE4.2: %s\n", SDL_HasSSE42() ? "true" : "false");
 
     // Request at least 32-bit color
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
@@ -36,9 +105,13 @@ main(int argc, char* argv[])
         "SDL2 OpenGL Demo",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        WINDOW_WIDTH,
-        WINDOW_HEIGHT,
+        640,
+        640,
         SDL_WINDOW_OPENGL);
+
+// TODO: add a flag for this
+//        SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP);
+
     if (window == NULL) {
         SDL_Quit();
 
@@ -62,17 +135,63 @@ main(int argc, char* argv[])
     printf("GLSL Version:    %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
     // Enable v-sync (set 1 to enable, 0 to disable)
-    SDL_GL_SetSwapInterval(1);
+    // TODO: add a flag for this
+    SDL_GL_SetSwapInterval(0);
 
     // Load the modern OpenGL funcs
     opengl_load_functions();
 
-    // TODO: do modern OpenGL stuff
+
+    // Do modern OpenGL stuff
+    const GLchar *vs_source =
+        "#version 330\n"
+        "layout(location = 0) in vec2 point;\n"
+        "uniform float angle;\n"
+        "void main() {\n"
+        "    mat2 rotate = mat2(cos(angle), -sin(angle),\n"
+        "                       sin(angle), cos(angle));\n"
+        "    gl_Position = vec4(0.75 * rotate * point, 0.0, 1.0);\n"
+        "}\n";
+    const GLchar *fs_source =
+        "#version 330\n"
+        "out vec4 color;\n"
+        "void main() {\n"
+        "    color = vec4(1, 0.15, 0.15, 0);\n"
+        "}\n";
+
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
     GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    printf("%d %d\n", vs, fs);
+    opengl_shader_compile_source(vs, vs_source);
+    opengl_shader_compile_source(fs, fs_source);
+
+    GLuint prog = glCreateProgram();
+    opengl_shader_link_program(prog, vs, fs);
+
+    GLint uniform_angle = glGetUniformLocation(prog, "angle");
+
     glDeleteShader(fs);
     glDeleteShader(vs);
+
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(SQUARE), SQUARE, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    long frame_count = 0;
+    unsigned long last_frame = 0;
+    unsigned long last_second = 0;
+    double angle = 0.0;
 
     bool running = true;
     while (running) {
@@ -84,7 +203,38 @@ main(int argc, char* argv[])
                 if (key == SDLK_q || key == SDLK_ESCAPE) running = false;
             }
         }
+
+        glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(prog);
+        glUniform1f(uniform_angle, angle);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, sizeof(SQUARE) / sizeof(*SQUARE) / 2);
+        glBindVertexArray(0);
+        glUseProgram(0);
+
+        unsigned long now = SDL_GetTicks();
+        unsigned long diff = now - last_frame;
+        last_frame = now;
+
+        angle += diff / 1000.0;
+        if (angle > 2 * M_PI) angle -= 2 * M_PI;
+
+        if (now - last_second >= 1000) {
+            printf("FPS: %ld\n", frame_count);
+            frame_count = 0;
+            last_second = now;
+        }
+
+        frame_count++;
+        SDL_GL_SwapWindow(window);
     }
+
+    // Cleanup OpenGL resources
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(prog);
 
     // Cleanup SDL2 resources
     SDL_GL_DeleteContext(context);
